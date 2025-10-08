ccc00f378487273ecbdeaacb08b20d462af12187
Fahim <jameefahim@gmail.com>
2025-10-07 20:04:33 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\nMy Mini Java Projects: ");
+        System.out.println("*************************************\n");
+        phoneNumberGenerator ph = new phoneNumberGenerator();
+        System.out.println("Random Phone Number generator: " + phoneNumberGenerator.generatorPhoneNumber());
+
+        //TODO: Create a while loop to ask user which one to run
+    }
+}
---
+++ src/phoneNumberGenerator.java
@@ -0,0 +1,29 @@
+
+import java.security.SecureRandom;
+
+public class phoneNumberGenerator {
+    public static StringBuilder generatorPhoneNumber() {
+        StringBuilder phoneNumber = new StringBuilder();
+
+        SecureRandom secureRandom = new SecureRandom();
+        //secureRandom.setSeed(59); // A seed determines the starting point of the random number sequence.
+
+        for (int i = 0; i < 3; i++) {
+            phoneNumber.append(String.valueOf(secureRandom.nextInt(8)));
+        }
+        phoneNumber.append("-");
+        for (int i = 0; i < 3; i++) {
+            phoneNumber.append(String.valueOf(secureRandom.nextInt(10)));
+        }
+        phoneNumber.append("-");
+        for (int i = 0; i < 4; i++) {
+            phoneNumber.append(String.valueOf(secureRandom.nextInt(10)));
+        }
+        return phoneNumber;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
